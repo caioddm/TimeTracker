66e765d060509faa8878745ef2f7b8d29afcbe91
caioddm <caioduarte.diniz@gmail.com>
2014-10-23 16:39:42 -0500

    MVC app skeleton
    First code commit with app skeleton

---
+++ code/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Code::Application.config.secret_token = 'a8841e98c65b20b4c3c0efb6b14e11c677cf41cb238aa4d30d1f176b19f1b0562d7427bd9660494adbfa04ca489b3fd5c940b66f8748bc04be34a2c8c8bcbfee'
