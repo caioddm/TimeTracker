a93921e06add04e18315ed068a920d6ae3aef5ea
meenakshi-n <meen92@tamu.edu>
2014-11-20 19:35:36 -0600

    Requests listed when signed in as admin, and they can approved/rejected. Still working on deletingthe timesheet record created if the admin decides to reject a request after approving it

--- code/app/models/timesheet.rb
+++ code/app/models/timesheet.rb
@@ -1,4 +1,4 @@
 class Timesheet < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :clockin, :clockout, :date, :minutes, :request_type
+  attr_accessible :clockin, :clockout, :date, :minutes, :request_type,:user
 end
