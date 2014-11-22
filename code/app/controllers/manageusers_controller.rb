bfbcfd14c31b83d88ea00021a4a29e13c0d68186
Anurag Garg <anuraggarg04@gmail.com>
2014-11-22 13:41:49 -0600

    Delete User

--- code/app/controllers/manageusers_controller.rb
+++ code/app/controllers/manageusers_controller.rb
@@ -16,5 +16,12 @@ class ManageusersController < SecureController
 	def index
 		@users = User.all
 	end
+	
+	def destroy
+	  @user = User.find(params[:id])
+	  @user.destroy
+	  
+	  redirect_to manageusers_path
+	end
 end
 	
