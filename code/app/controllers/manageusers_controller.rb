4aa6700f4bcb910d3076946403dbf2e73ed5f79e
Anurag Garg <anuraggarg04@gmail.com>
2014-11-23 01:37:25 -0600

    Edit User Functionality

--- code/app/controllers/manageusers_controller.rb
+++ code/app/controllers/manageusers_controller.rb
@@ -1,28 +1,38 @@
 class ManageusersController < SecureController
-	def new
-		
-	end	
-	
-	def authenticate_user!
-	end
-	
-	def create
-		@user = User.new(params[:user])
-		@user.save
-		redirect_to manageusers_path
-		
-	end
-	
-	def index
-		@users = User.where("is_user_active = ?", true)
-	end
-	
-	def destroy
-	  @user = User.find(params[:id])
-	  @user.is_user_active = false
-	  @user.save
-	  
-	  redirect_to manageusers_path
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to manageusers_path
+
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    ui_user = params[:user]
+    ui_user.delete(:password)
+    ui_user.delete(:password_confirmation)
+    #calling update_attributes for validation checks
+    if @user.update_attributes(ui_user)
+      redirect_to manageusers_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  def index
+    @users = User.where("is_user_active = ?", true)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.is_user_active = false
+    @user.save
+
+    redirect_to manageusers_path
+  end
 end
-	
