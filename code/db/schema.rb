4ad5555273f45aa856d774e77d484fdedd9cb5ee
Anurag Garg <anuraggarg04@gmail.com>
2014-11-23 23:38:46 -0600

    1. Added Phone 2. Migration 3. UI Changes

--- code/db/schema.rb
+++ code/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141122223844) do
+ActiveRecord::Schema.define(:version => 20141124051257) do
 
   create_table "requests", :force => true do |t|
     t.date     "date"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20141122223844) do
     t.string   "last_sign_in_ip"
     t.boolean  "is_admin"
     t.boolean  "is_user_active",         :default => true
+    t.string   "phone"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
