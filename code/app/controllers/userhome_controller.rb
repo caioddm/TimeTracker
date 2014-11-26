fd875df51a630a11f5deddaa2d9c301ed639ef14
github-anurag <anurag1106@tamu.edu>
2014-11-26 16:01:28 -0600

    Pulled code out of view

--- code/app/controllers/userhome_controller.rb
+++ code/app/controllers/userhome_controller.rb
@@ -2,18 +2,7 @@ class UserhomeController < SecureController
   def index
     if current_user.is_admin == true
       redirect_to :controller=>'adminhome', :action => 'index' and return
-          
-    if current_user
-      @user = current_user
-      @timesheets= @user.timesheets
-      respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user } 
-    end  
-    else
-      redirect_to new_user_session_path, notice: 'You are not logged in.' and return
-    end
-   
-    end
+    end      
+      @timesheets= current_user.timesheets
   end
 end
