a6de55851fda16e6441600f1f236d5e6bf45645f
Somaieh <somaieh.bahrami@gmail.com>
2014-12-09 18:21:34 -0600

    Revert "Revert "Anurag:Order Users by last Name""
    This reverts commit 885433349d8c36763d2a5f831da464f5f5cf3d67.

--- code/app/controllers/adminhome_controller.rb
+++ code/app/controllers/adminhome_controller.rb
@@ -6,6 +6,6 @@ class AdminhomeController < AdminController
     period = Timesheet.get_period start_period, end_period
     @start_period = period[0]
     @end_period = period[1]
-    @timesheet = Timesheet.joins(:user).where("date >= ? AND date <= ?", @start_period, @end_period).group([:user_id,:request_type]).select("user_id, sum(minutes) as minutes,request_type").order(:name)
+    @timesheet = Timesheet.joins(:user).where("date >= ? AND date <= ?", @start_period, @end_period).group([:user_id,:request_type]).select("user_id, sum(minutes) as minutes,request_type").order(:last_name,:name)
   end
 end
